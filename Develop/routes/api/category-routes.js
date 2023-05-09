a465526f15bdbf0c94e72496e7d3f198212bb0dc
PopGirma1 <girmaytadesepop@gmail.com>
2023-05-09 06:55:45 +0300

    first commit

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,107 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    }
+  )
+    .then(categoryInfo => res.json(categoryInfo))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['category_id']
+    }
+  })
+    .then(categoryInfo => res.json(categoryInfo))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(categoryInfo => res.json(categoryInfo))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryInfo => {
+      if (!categoryInfo) {
+        res.status(404).json({ message: 'There is no Category with this ID.' });
+        return;
+      }
+      res.json(categoryInfo);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(categoryInfo => {
+      if (!categoryInfo) {
+        res.status(404).json({ message: 'There is no Category with this ID' });
+        return;
+      }
+      res.json(categoryInfo,{message:"Category name deleted successfully "});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+    
+});
+
+module.exports = router;
---
+++ README.md
@@ -0,0 +1,58 @@
+# E-commerce Back End 
+The challenge is to build the back end for an e-commerce site.
+
+## description
+This app needs a working Express.js API and configure it to use Sequelize to interact with a MySQL database. This application won’t be deployed so I will show a walkthrough video that demonstrates its functionality.
+## Technologies
+        Javascript
+        Node.js
+        Sequelize
+        MySQL2
+        Express
+        Dotenv
+## Installation
+        step 1 :git clone https://github.com/AbelZemo/E-commerce-Back-End 
+        step 2 : Both Node.js and MySQL must be installed on your computer.
+        step 3 : Install dependencies using the below commands
+        ( 
+                npm init --y
+                npm install express sequelize mysql2
+        )
+        step 4 : Open up MySQL shell and type this input (source db/schema.sql)
+        step 5 : type this input (use ecommerce_db)
+        step 6 : Then quit MySQL shell and input the following command in your terminal (npm run seed)
+        step 7 : to start running application simply type this command 
+        (
+                npm start or node server.js
+        )
+        step 8 : Open up Insomnia core to GET, POST, PUT and DELETE from different routes.
+
+## screenshots
+
+![1](screenshots/1.png)
+![2](screenshots/2.png)
+![3](screenshots/3.png)
+![4](screenshots/4.png)
+![5](screenshots/5.png)
+![6](screenshots/6.png)
+![1](screenshots/7.png)
+![2](screenshots/8.png)
+![3](screenshots/9.png)
+![4](screenshots/10.png)
+![5](screenshots/11.png)
+![6](screenshots/12.png)
+![1](screenshots/13.png)
+![2](screenshots/14.png)
+![3](screenshots/15.png)
+![4](screenshots/16.png)
+
+
+## Usage
+The application is used to GET data for each route(categories, products, or tags) as well as create, update, and delete data in those routes.
+
+
+## walkthrough video link
+
+
+
+
